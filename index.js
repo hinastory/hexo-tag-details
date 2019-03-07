0c51d36e7125c7aae8e9c3b37172c11aa44547a2
hinastory <1696779+hinastory@users.noreply.github.com>
2019-03-08 07:56:38 +0900

    tweak code

--- index.js
+++ index.js
@@ -13,16 +13,16 @@
 const util = require('hexo-util');
 const config = hexo.config.tagDetails;
 const className = (config && config.className) ? config.className: false;
-const openSetting = (config && config.openSetting) ? config.openSetting: false;
+const openSetting = (config && config.open) ? config.open: false;
 
 hexo.extend.tag.register('details', tagDetails, { ends: true });
 
 function tagDetails(args, content) {
   let isOpen = e => e === "mode:open"
-  let isClose = e => e === "mode:Close"
+  let isClose = e => e === "mode:close"
   let isMode = e => isOpen(e) || isClose(e);
 
-  let filtered = args.filter(e => isMode(e));
+  let filtered = args.filter(e => !isMode(e));
   let modeFlag = isOpen(args.find(e => isMode(e)));
 
   let openMode = filtered.length < args.length ? modeFlag : openSetting;
